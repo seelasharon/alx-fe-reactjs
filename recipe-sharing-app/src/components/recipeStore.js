eb155698a917510f694f79c55dcf59a0765bd1ae
sharon seela <sharonshukuru58@gmail.com>
2025-11-16 15:07:46 +0300

    Update recipe store with updateRecipe and deleteRecipe actions

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,14 +1,27 @@
-import { create } from 'zustand'
+import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
 
+  // Add a new recipe
   addRecipe: (newRecipe) =>
     set((state) => ({
       recipes: [...state.recipes, newRecipe],
     })),
 
-  setRecipes: (recipes) => set({ recipes }),
+  // Delete a recipe by ID
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
+  // Update a recipe (by ID)
+  updateRecipe: (id, updatedFields) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedFields } : recipe
+      ),
+    })),
 }));
 
 export default useRecipeStore;
