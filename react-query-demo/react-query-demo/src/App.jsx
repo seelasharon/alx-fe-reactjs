4a315fecceeb23aeacc83351d64663be14b91d6b
sharon seela <sharonshukuru58@gmail.com>
2025-12-14 12:59:29 +0300

    Set up React Query with data fetching, caching, and refetching

---
+++ react-query-demo/react-query-demo/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import PostsComponent from './PostsComponent';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
